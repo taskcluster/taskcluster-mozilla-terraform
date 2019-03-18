e6eabbaa827aac747ea70c996229bc113b74c608
Dustin J. Mitchell <dustin@mozilla.com>
2019-03-18 12:38:49 -0400

    Bug 1533591 - display websocktunnel_secret as an output

--- tf/taskcluster/outputs.tf
+++ tf/taskcluster/outputs.tf
@@ -3,6 +3,11 @@ output "root_access_token" {
   value     = "${module.taskcluster.root_access_token}"
 }
 
+output "websocktunnel_secret" {
+  sensitive = true
+  value     = "${module.taskcluster.websocktunnel_secret}"
+}
+
 output "cluster_ip" {
   value = "${data.kubernetes_service.deployment_endpoint.load_balancer_ingress.0.ip}"
 }
